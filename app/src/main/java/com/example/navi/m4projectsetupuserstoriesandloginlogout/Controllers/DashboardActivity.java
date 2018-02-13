755e0c122188b4d84d504baf3ed574ad03bdf0a8
tjbrownlow8 <tbrownlow8@gatech.edu>
2018-02-13 18:02:39 -0500

    Added logout functionality
    Made the logout function work

--- app/src/main/java/com/example/navi/m4projectsetupuserstoriesandloginlogout/Controllers/DashboardActivity.java
+++ app/src/main/java/com/example/navi/m4projectsetupuserstoriesandloginlogout/Controllers/DashboardActivity.java
@@ -1,15 +1,19 @@
 package com.example.navi.m4projectsetupuserstoriesandloginlogout.Controllers;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 import com.example.navi.m4projectsetupuserstoriesandloginlogout.R;
 
 public class DashboardActivity extends AppCompatActivity {
+
+    private Button logoutButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,16 +22,15 @@ public class DashboardActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        if (fab != null) {
-            fab.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                            .setAction("Action", null).show();
-                }
-            });
-        }
+
+        logoutButton = (Button) findViewById(R.id.logoutButton);
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent welcomeScreenIntent = new Intent(DashboardActivity.this, com.example.navi.m4projectsetupuserstoriesandloginlogout.Controllers.WelcomeScreen.class);
+                startActivity(welcomeScreenIntent);
+            }
+        });
     }
 
 }

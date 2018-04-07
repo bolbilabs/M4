4d5330e0274a5bd90ba263089b5a8e0319ffac92
Duo-Wei Yang <duoweiyang@gmail.com>
2018-04-07 17:44:49 -0400

    JUnit test wasn't working
    Made new variable to make sure it worked.

--- app/src/test/java/com/example/navi/m4projectsetupuserstoriesandloginlogout/Models/ShelterEqualTest.java
+++ app/src/test/java/com/example/navi/m4projectsetupuserstoriesandloginlogout/Models/ShelterEqualTest.java
@@ -2,7 +2,6 @@ package com.example.navi.m4projectsetupuserstoriesandloginlogout.Models;
 
 import org.junit.Before;
 import org.junit.Test;
-
 import static org.junit.Assert.*;
 
 /**
@@ -11,6 +10,7 @@ import static org.junit.Assert.*;
 public class ShelterEqualTest {
 
     private Shelter shelterName;
+    private Shelter testName;
 
     @Before
     public void setUp() throws Exception {
@@ -20,10 +20,18 @@ public class ShelterEqualTest {
 
     @Test
     public void equals() throws Exception {
-        assertFalse("Comparison fails for null", shelterName.equals(null));
-        assertFalse("It's not a shelter", shelterName.equals(""));
-        assertTrue("It does have the same shelter name", shelterName.equals("Shelter Bob"));
-        assertFalse("The shelter name is different from the actual one", shelterName.equals("Shelter Sally"));
+        testName = new Shelter("1", null, "2", "3", "4", "5",
+                "6", "7", "8");
+        assertFalse("Comparison fails for null", shelterName.equals(testName));
+        testName = new Shelter("1", "", "2", "3", "4", "5",
+                "6", "7", "8");
+        assertFalse("It's not a shelter", shelterName.equals(testName));
+        testName = new Shelter("1", "Shelter Bob", "2", "3", "4", "5",
+                "6", "7", "8");
+        assertTrue("It does have the same shelter name", shelterName.equals(testName));
+        testName = new Shelter("1", "Shelter Sally", "2", "3", "4", "5",
+                "6", "7", "8");
+        assertFalse("The shelter name is different from the actual one", shelterName.equals(testName));
         // Need to make sure it's true if it's the same shelter shelterName.equals(shelterName)
         // Need to make sure if it's a shelter with a different name, it's false. Make up something new
         //with this one.
